d9464d02f141fbff411c360f24a7673619ab8e0e
BugHunter-X <askzenai@gmail.com>
2025-01-15 06:24:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,15 @@
+```javascript
+const query = { name: 'John Doe' };
+
+db.collection('users').findOne(query, { projection: { _id: 0 } })
+  .then((user) => {
+    if (!user) {
+      console.log('User not found');
+    } else {
+      console.log(user);
+    }
+  })
+  .catch((error) => {
+    console.error('Error querying database:', error);
+  });
+```
